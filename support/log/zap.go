afb9a81c31cd3b7922c86dec07063a83f8b08374
nthota <nthota@tibco.com>
2019-02-05 17:20:22 +0530

    default configuration moved to separate file

--- support/log/zap.go
+++ support/log/zap.go
@@ -181,16 +181,16 @@ func newZapRootLogger(name string) Logger {
 
 func newZapLogger() (*zap.Logger, *zap.AtomicLevel, error) {
 
-	zl, err := zapcores.GetDefaultLogConfig().Build(zap.AddCallerSkip(1))
+	zl, err := zapcores.GetDefConfig().GetDefaultLogConfig().Build(zap.AddCallerSkip(1))
 
-	return zl, zapcores.GetDefaultLogLevl(), err
+	return zl, zapcores.GetDefConfig().GetDefaultLogLevl(), err
 }
 
 func newZapTraceLogger() (*zap.Logger, *zap.AtomicLevel, error) {
 
-	zl, err := zapcores.GetDefaultTraceLogConfig().Build(zap.AddCallerSkip(1))
+	zl, err := zapcores.GetDefConfig().GetDefaultTraceLogConfig().Build(zap.AddCallerSkip(1))
 
-	return zl, zapcores.GetDefaultTraceLogLevl(), err
+	return zl, zapcores.GetDefConfig().GetDefaultTraceLogLevl(), err
 }
 
 func newZapChildLogger(logger Logger, name string) (Logger, error) {
--- support/log/zapcores/zapcores.go
+++ support/log/zapcores/zapcores.go
@@ -1,20 +1,7 @@
 package zapcores
 
 import (
-	"os"
-	"strings"
-
-	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-)
-
-type Format int
-
-const (
-	EnvKeyLogFormat         = "FLOGO_LOG_FORMAT"
-	DefaultLogFormat        = FormatConsole
-	FormatConsole    Format = iota
-	FormatJSON
 )
 
 // zapCoreMap holds logger core impl
@@ -23,24 +10,7 @@ var zapCoreMap map[string]zapcore.Core
 // zapTraceCoreMap holds trace logger core impl
 var zapTraceCoreMap map[string]zapcore.Core
 
-// logCfg Default log configuration for core creation
-var logCfg zap.Config
-
-// traceLogCfg Default trace log configuration for core creation
-var traceLogCfg zap.Config
-
-// logLvl log level variable which will control type of logs
-var logLvl *zap.AtomicLevel
-
-// traceLogLvl log level variable which will control type of logs
-var traceLogLvl *zap.AtomicLevel
-
 func init() {
-
-	createDefaultConfiguration()
-
-	createTraceConfiguration()
-
 	zapCoreMap = make(map[string]zapcore.Core)
 	zapTraceCoreMap = make(map[string]zapcore.Core)
 }
@@ -63,75 +33,4 @@ func GetZapCoreMap() map[string]zapcore.Core {
 // GetZapTraceCoreMap returns complete trace log core map
 func GetZapTraceCoreMap() map[string]zapcore.Core {
 	return zapTraceCoreMap
-}
-
-func GetDefaultLogConfig() zap.Config {
-	return logCfg
-}
-
-func GetDefaultLogLevl() *zap.AtomicLevel {
-	return logLvl
-}
-
-func GetDefaultTraceLogConfig() zap.Config {
-	return traceLogCfg
-}
-
-func GetDefaultTraceLogLevl() *zap.AtomicLevel {
-	return traceLogLvl
-}
-
-func createDefaultConfiguration() {
-
-	logFormat := DefaultLogFormat
-	envLogFormat := strings.ToUpper(os.Getenv(EnvKeyLogFormat))
-	if envLogFormat == "JSON" {
-		logFormat = FormatJSON
-	}
-
-	cfg := zap.NewProductionConfig()
-	cfg.DisableCaller = true
-
-	eCfg := cfg.EncoderConfig
-	eCfg.TimeKey = "timestamp"
-	//eCfg.EncodeTime = zapcore.ISO8601TimeEncoder
-	eCfg.EncodeTime = zapcore.EpochNanosTimeEncoder
-
-	if logFormat == FormatConsole {
-		eCfg.EncodeLevel = zapcore.CapitalLevelEncoder
-		cfg.Encoding = "console"
-		eCfg.EncodeName = nameEncoder
-	}
-
-	cfg.EncoderConfig = eCfg
-
-	lvl := cfg.Level
-
-	// assign single lvl instance to global level
-	logLvl = &lvl
-
-	// assigning default configuration to global Cfg
-	logCfg = cfg
-}
-
-func createTraceConfiguration() {
-	cfg := logCfg
-
-	if strings.Compare(cfg.Encoding, "console") == 0 {
-		cfg.EncoderConfig.EncodeLevel = traceLevelEncoder
-	}
-
-	lvl := cfg.Level
-	lvl.SetLevel(zapcore.DebugLevel)
-
-	traceLogCfg = cfg
-	traceLogLvl = &lvl
-}
-
-func nameEncoder(loggerName string, enc zapcore.PrimitiveArrayEncoder) {
-	enc.AppendString("[" + loggerName + "] -")
-}
-
-func traceLevelEncoder(l zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
-	enc.AppendString("[TRACE]")
 }
